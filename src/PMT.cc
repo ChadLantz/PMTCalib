7af6cf3b030183d080df624c5af23ea59f7da33a
Chad Lantz <clantz2@illinois.edu>
2026-06-29 11:35:53 -0500

    Removed internal TH1. Now produces a new TH1 on each GenSpectrum call. Moved back to sper TF1 for GetRandom

--- src/PMT.cc
+++ src/PMT.cc
@@ -1,7 +1,7 @@
 #include "PMT.h"
 
 
-#include "SPEResponseFactory.h"
+#include "SPEResponse.h"
 #include "TRandom.h"
 
 ClassImp(PMT)
@@ -12,41 +12,37 @@ ClassImp(PMT)
 
 PMT::~PMT() {}
 
-PMT::PMT(Int_t _nbins, Double_t _min, Double_t _max, Pedestal _ped, PMType::Response _res, Double_t *_params)
+PMT::PMT(Int_t _nbins, Double_t _min, Double_t _max, Double_t Q0, Double_t s0, PMType::Response _res, Double_t *_params)
+: m_nbins(_nbins),
+   m_min(_min),
+   m_max(_max),
+   m_ped(Q0, s0),
+   m_res(_res)
 {
-   nbins = _nbins;
-   min = _min;
-   max = _max;
+   m_res.SetParameters(_params);
+   m_spefunc = new TF1("pmt", m_res, m_min, m_max, m_res.NPar());
+   m_spefunc->SetParameters(_params);
+}
 
-   ped = _ped;
-   SPEResponseFactory sperf;
-   res = sperf.Build(_res, _params);
-
-   spectrum = new TH1D("hspectrum", "PMT spectrum; Charge [AU]; Entries", nbins, min, max);
-
+TH1D *PMT::GenSpectrum(Int_t ntot, Double_t mu)
+{
+   TH1D *spectrum = new TH1D("hspectrum", "PMT spectrum; Charge [AU]; Entries", m_nbins, m_min, m_max);
    spectrum->SetMarkerStyle(20);
    spectrum->SetMarkerSize(0.75);
    spectrum->SetLineColor(kBlack);
    spectrum->SetMarkerColor(kBlack);
-}
-
-void PMT::GenSpectrum(Int_t ntot, Double_t mu)
-{
    spectrum->Reset();
-
    gRandom->SetSeed(0);
 
    for (Int_t i = 0; i < ntot; i++) {
-      Double_t q = ped.GenQ();
-
+      Double_t q = m_ped.GenQ();
       Int_t npe = gRandom->Poisson(mu);
-
       for (Int_t j = 0; j < npe; j++) {
-         q += res.GetRandom();
+         q += m_spefunc->GetRandom();
       }
 
       spectrum->Fill(q);
    }
 
-   return;
+   return spectrum;
 }
--- src/PMT.h
+++ src/PMT.h
@@ -1,32 +1,27 @@
-
 #ifndef PMT_H
 #define PMT_H
 
 #include "PMType.h"
-#include "TObject.h"
+#include "SPEResponse.h"
 #include "TH1.h"
 
 #include "Pedestal.h"
-
-class PMT : public TObject {
-private:
-   TH1D *spectrum;
-   Int_t nbins;
-   Double_t min;
-   Double_t max;
-   Pedestal ped;
-   TF1 res;
-
+class PMT {
 public:
    PMT();
-   PMT(Int_t _nbins, Double_t _min, Double_t _max, Pedestal _ped, PMType::Response _res, Double_t *_params);
+   PMT(const PMT &other);
+   PMT(Int_t _nbins, Double_t _min, Double_t _max, Double_t Q0, Double_t s0, PMType::Response _res, Double_t *_params);
    virtual ~PMT();
 
-   void GenSpectrum(Int_t ntot, Double_t mu);
-   TH1D *GetSpectrum() { return spectrum; };
-   void DrawSpectrum() { spectrum->Draw("PEZ"); };
+   TH1D *GenSpectrum(Int_t ntot, Double_t mu);
 
-   ClassDef(PMT, 1)
+private:
+   Int_t m_nbins;
+   Double_t m_min;
+   Double_t m_max;
+   Pedestal m_ped;
+   SPEResponse m_res;
+   TF1 *m_spefunc{nullptr};
 };
 
 #endif
