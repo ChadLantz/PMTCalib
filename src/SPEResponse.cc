1078f084731a7b2553410a7cbbf99b118daad3ee
Chad Lantz <clantz2@illinois.edu>
2026-07-02 14:40:09 -0500

    Added Gain and GainError functions

--- src/SPEResponse.cc
+++ src/SPEResponse.cc
@@ -5,25 +5,21 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 /// Default constructor
-SPEResponse::SPEResponse()
-: IModel(), m_resp(PMType::Response::GAUSS)
+SPEResponse::SPEResponse() : IModel(), m_resp(PMType::Response::GAUSS)
 {
    SetResponse(m_resp);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 /// Main constructor
-SPEResponse::SPEResponse(PMType::Response resp)
-: IModel()
+SPEResponse::SPEResponse(PMType::Response resp) : IModel()
 {
    SetResponse(resp);
 }
 
-
 ////////////////////////////////////////////////////////////////////////////////
 /// Copy constructor
-SPEResponse::SPEResponse(const SPEResponse &other)
-: IModel(other), m_resp(other.m_resp)
+SPEResponse::SPEResponse(const SPEResponse &other) : IModel(other), m_resp(other.m_resp)
 {
    SetResponse(m_resp);
 }
@@ -72,7 +68,7 @@ void SPEResponse::SetResponse(PMType::Response resp)
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-/// 
+///
 Double_t SPEResponse::gausexpfunc(Double_t x, const Double_t *par) const
 {
    Double_t Q = par[0];
@@ -98,7 +94,7 @@ Double_t SPEResponse::gausexpfunc(Double_t x, const Double_t *par) const
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-/// 
+///
 Double_t SPEResponse::gaus2expfunc(Double_t x, const Double_t *par) const
 {
    Double_t Q = par[0];
@@ -126,7 +122,7 @@ Double_t SPEResponse::gaus2expfunc(Double_t x, const Double_t *par) const
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-/// 
+///
 Double_t SPEResponse::gammaexpfunc(Double_t x, const Double_t *par) const
 {
    Double_t lambda = par[0];
@@ -147,7 +143,7 @@ Double_t SPEResponse::gammaexpfunc(Double_t x, const Double_t *par) const
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-/// 
+///
 Double_t SPEResponse::gamma2expfunc(Double_t x, const Double_t *par) const
 {
    Double_t lambda = par[0];
@@ -170,7 +166,7 @@ Double_t SPEResponse::gamma2expfunc(Double_t x, const Double_t *par) const
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-/// 
+///
 Double_t SPEResponse::weibullexpfunc(Double_t x, const Double_t *par) const
 {
    Double_t lambda = par[0];
@@ -190,7 +186,7 @@ Double_t SPEResponse::weibullexpfunc(Double_t x, const Double_t *par) const
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-/// 
+///
 Double_t SPEResponse::lognormalexpfunc(Double_t x, const Double_t *par) const
 {
    Double_t Q = par[0];
@@ -214,7 +210,7 @@ Double_t SPEResponse::lognormalexpfunc(Double_t x, const Double_t *par) const
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-/// 
+///
 Double_t SPEResponse::testfunc(Double_t x, const Double_t *par) const
 {
    Double_t Q = par[0];
@@ -244,7 +240,69 @@ Double_t SPEResponse::testfunc(Double_t x, const Double_t *par) const
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-/// 
+/// Return the gain
+Double_t SPEResponse::Gain(const Double_t *pars) const
+{
+   Double_t result = 0.0;
+   Double_t Q = pars[0];
+   Double_t lambda = pars[0];
+   Double_t alpha = pars[2];
+   Double_t w = pars[3];
+   switch (m_resp) {
+   case PMType::Response::GAUSS:
+   case PMType::Response::LOGNORMAL:
+   case PMType::Response::GAUSS2EXP:      // Until I know what the calculation is
+      result = w / alpha + (1.0 - w) * Q; // From PMTCalib example2
+
+   case PMType::Response::GAMMA:
+   case PMType::Response::WEIBULL:
+   case PMType::Response::GAMMA2EXP: result = w / alpha + (1.0 - w) / lambda;
+
+   case PMType::Response::TEST: result = 0.0;
+   }
+   Info("SPEResponse::GainError", "\n\tQ/theta\t: %.2e\nalpha\t: %.2e\nw\t: %.2e\ngainError\t: %.2e\n", Q, alpha, w,
+        result);
+   return result;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// Return the error on the gain const method
+Double_t SPEResponse::GainError(const Double_t *pars, const Double_t *errs) const
+{
+   Double_t result = 0.0;
+   Double_t Q = pars[0];
+   Double_t lambda = pars[0];
+   Double_t alpha = pars[2];
+   Double_t w = pars[3];
+   Double_t Qerr = errs[0];
+   Double_t lambdaErr = errs[0];
+   Double_t alphaErr = errs[2];
+   Double_t wErr = errs[3];
+   switch (m_resp) {
+   case PMType::Response::GAUSS:
+   case PMType::Response::LOGNORMAL:
+   case PMType::Response::GAUSS2EXP: // Until I know what the calculation is
+      result = TMath::Sqrt(TMath::Power((1.0 - w) * Qerr, 2.0) + TMath::Power((Q - alpha) * wErr, 2.0) +
+                           TMath::Power(w / (alpha * alpha) * alphaErr, 2));
+      break;
+
+   case PMType::Response::GAMMA:
+   case PMType::Response::WEIBULL:
+   case PMType::Response::GAMMA2EXP:
+      result = TMath::Sqrt(TMath::Power(wErr * (1 / alpha - 1 / lambda), 2.0) +
+                           TMath::Power(-alphaErr * w / TMath::Power(alpha, 2.0), 2.0) +
+                           TMath::Power(-lambdaErr * (1 - w) / TMath::Power(lambda, 2.0), 2.0));
+
+      break;
+
+   case PMType::Response::TEST: result = 0.0;
+   }
+
+   return 0.0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+///
 Double_t SPEResponse::DoEvalPar(Double_t x, const Double_t *pars) const
 {
    Double_t retVal = 0.0;
@@ -256,9 +314,7 @@ Double_t SPEResponse::DoEvalPar(Double_t x, const Double_t *pars) const
    case PMType::Response::GAUSS2EXP: retVal = gaus2expfunc(x, pars); break;
    case PMType::Response::GAMMA2EXP: retVal = gamma2expfunc(x, pars); break;
    case PMType::Response::TEST: retVal = testfunc(x, pars); break;
-   default:
-      Error("SPEResponse::DoEvalPar", "Invalid response type selected m_resp = %d", m_resp);
-      break;
+   default: Error("SPEResponse::DoEvalPar", "Invalid response type selected m_resp = %d", m_resp); break;
    }
 
    // Info("SPEResponse::gammaexpfunc", "Returned %.2f for x = %.2f", retVal, x);
