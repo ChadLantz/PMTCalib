4876dcfee730ff7835fb967587b1768a0ccdfac3
Chad Lantz <clantz2@illinois.edu>
2026-09-23 16:19:19 -0500

    Add enums and string conversion functions for Method, Response, and Model types

--- src/PMType.h
+++ src/PMType.h
@@ -2,7 +2,16 @@
 #ifndef PMTYPE_H
 #define PMTYPE_H
 
+#include <array>
+
 namespace PMType {
+enum Method{
+   NumIntegration,
+   DFTmethod,
+   PMTModel,
+};
+static std::array<Method, 3> AllMethods{{NumIntegration, DFTmethod, PMTModel}};
+
 enum Response {
    GAUSS,
    GAMMA,
@@ -12,6 +21,7 @@ enum Response {
    GAMMA2EXP,
    TEST
 };
+static std::array<Response, 7> AllResponseTypes{{GAUSS, GAMMA, WEIBULL, LOGNORMAL, GAUSS2EXP, GAMMA2EXP, TEST}};
 
 enum Model {
    SIMPLEGAUSS,
@@ -19,6 +29,42 @@ enum Model {
    ANATRUNCG,
    EXPTRUNCG
 };
+static std::array<Model, 4> AllModels{{SIMPLEGAUSS, TRUNCGAUSS, ANATRUNCG, EXPTRUNCG}};
+
+static inline const char *ToString(Method method)
+{
+   switch (method) {
+   case NumIntegration: return "NumIntegration";
+   case DFTmethod: return "DFTmethod";
+   case PMTModel: return "PMTModel";
+   }
+   return "Invalid Response";
+}
+
+static inline const char *ToString(Response response)
+{
+   switch (response) {
+   case GAUSS: return "GAUSS";
+   case GAMMA: return "GAMMA";
+   case WEIBULL: return "WEIBULL";
+   case LOGNORMAL: return "LOGNORMAL";
+   case GAUSS2EXP: return "GAUSS2EXP";
+   case GAMMA2EXP: return "GAMMA2EXP";
+   case TEST: return "TEST";
+   }
+   return "Invalid Response";
+}
+
+static inline const char *ToString(Model model)
+{
+   switch (model) {
+   case SIMPLEGAUSS: return "SIMPLEGAUSS";
+   case TRUNCGAUSS: return "TRUNCGAUSS";
+   case ANATRUNCG: return "ANATRUNCG";
+   case EXPTRUNCG: return "EXPTRUNCG";
+   }
+   return "Invalid Model option";
+}
 
 } // namespace PMType
 
